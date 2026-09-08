20ae3ff26b40b23e3c26cca77d49da2f38c6054d
Millertimefit <107629317+Millertimefit@users.noreply.github.com>
2026-09-08 09:04:40 -0400

    Photo on the template form and thumbnail icons on Home

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "hwa-estate-v11";
-const FILES = ["./index.html", "./style.css", "./app.js", "./easy.js", "./schedule.js", "./catalog.js", "./manifest.json", "./icon.svg"];
+const CACHE = "hwa-estate-v12";
+const FILES = ["./index.html", "./style.css", "./pics.css", "./app.js", "./easy.js", "./schedule.js", "./catalog.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES).catch(() => {})));
